28c9f2e5179729e8eaa605d31c9b7adf7cc006f5
Levi Chinecherem Chidi <74856946+Levi-Chinecherem@users.noreply.github.com>
2024-06-10 00:59:09 +0100

    corrected cloudinary on view

--- tts_app/views.py
+++ tts_app/views.py
@@ -3,6 +3,7 @@
 from django.shortcuts import render
 from gtts import gTTS
 import cloudinary.uploader
+import tempfile
 
 def index(request):
     audio_url = None
@@ -10,6 +11,11 @@ def index(request):
         text = request.POST.get('text')
         if text:
             tts = gTTS(text)
-            audio_file = cloudinary.uploader.upload(tts, resource_type="raw")
-            audio_url = audio_file.get("secure_url")
+            # Create a temporary file to store the audio
+            with tempfile.NamedTemporaryFile(suffix='.mp3', delete=False) as temp_audio:
+                tts.write_to_fp(temp_audio)
+                temp_audio.close()
+                # Upload the temporary audio file to Cloudinary
+                audio_file = cloudinary.uploader.upload(temp_audio.name, resource_type="raw")
+                audio_url = audio_file.get("secure_url")
     return render(request, 'index.html', {'audio_url': audio_url})
