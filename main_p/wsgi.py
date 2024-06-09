8968ada6f9ccce87664f1caded89d7dfdd351f2d
Levi Chinecherem Chidi <74856946+Levi-Chinecherem@users.noreply.github.com>
2024-06-10 00:16:53 +0100

    collected static

--- main_p/wsgi.py
+++ main_p/wsgi.py
@@ -8,11 +8,16 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
 """
 
 import os
+from pathlib import Path
 
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+from whitenoise import WhiteNoise
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'main_p.settings')
 
 application = get_wsgi_application()
+application = WhiteNoise(application, root=os.path.join(BASE_DIR, 'staticfiles'))
 
 app = application
