17a90d0f83a18c7c0ca915db56ac288e05716923
Levi Chinecherem Chidi <74856946+Levi-Chinecherem@users.noreply.github.com>
2024-06-10 00:29:19 +0100

    added media settings

--- main_p/settings.py
+++ main_p/settings.py
@@ -122,6 +122,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
+# Media settings
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
--- tts_app/views.py
+++ tts_app/views.py
@@ -1,13 +1,18 @@
+# views.py
+
 from django.shortcuts import render
 from gtts import gTTS
 import os
+from django.conf import settings
 
-def index(request):
+def text_to_audio(request):
     audio_url = None
     if request.method == 'POST':
-        text = request.POST['text']
-        tts = gTTS(text)
-        audio_file_path = 'static/output.mp3'
-        tts.save(audio_file_path)
-        audio_url = 'static/output.mp3'
+        text = request.POST.get('text')
+        if text:
+            tts = gTTS(text)
+            audio_file = 'audio.mp3'
+            audio_path = os.path.join(settings.MEDIA_ROOT, audio_file)
+            tts.save(audio_path)
+            audio_url = settings.MEDIA_URL + audio_file
     return render(request, 'index.html', {'audio_url': audio_url})
